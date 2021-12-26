9f78bf9bcc901db139024ac6ad94b5eedc8a5b6e
test <test@test.com>
2021-12-26 14:14:15 +0530

    express server startup code setup.

---
+++ index.js
@@ -0,0 +1,32 @@
+// env file setup
+require('dotenv/config');
+
+// import express
+const express = require('express');
+const app = express();
+const port = process.env.PORT;
+const cors = require('cors');
+const morgan = require('morgan');
+
+
+// middleware setup
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// routes
+app.get('/', (req, res) => {
+  return res.status(200).json({
+    status: true,
+    code: 200,
+    message: "api route working."
+  });
+});
+
+
+// start web server
+app.listen(port, () => {
+  console.log(`Web server running at port: ${port}`);
+  console.log(`URL: http://localhost:${port}`);
+});
